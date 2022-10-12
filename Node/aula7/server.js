5bc72e281578822c3aaf5148c348aa27722e4a26
Bruno Magalhães <106349470+brunozer0@users.noreply.github.com>
2022-10-12 12:29:48 -0300

    Express
    Atualização que esqueci de fazer

---
+++ Node/aula7/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+//            criar ler atualizar apagar
+// -> CRUD = CREATE, READ, UPDATE, DELETE    
+//         =  POST ,  GET , PUT , DELETE
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action= "/" method= "POST">
+    Nome: <input type="text" name= "nome">
+    <button>Enviar</button>
+    </form>
+    `)
+})
+app.post('/', (req, res) => {
+    res.send('formulário recebido')
+})
+app.get('/contato', (req, res) => {
+    res.send('obrigado pelo contato')
+})
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000')
+console.log('servidor executando na porta 3000')
+})
