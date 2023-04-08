0c3406269369918a7a61acb334626c634348c5b8
Bruno Magalhaes <magalhaes.brunno1@gmail.com>
2023-04-07 22:33:02 -0300

    history

--- react/react-base/src/pages/Page404/index.js
+++ react/react-base/src/pages/Page404/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
+import history from '../../services/history';
 
 export default function Page404() {
+  history.push('/');
+
   return (
     <Container>
       <h1>Página não existe </h1>
